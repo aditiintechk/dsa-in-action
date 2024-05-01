5b6064cdfd0d33e521028adb8574432dd31dbcca
Aditi <aditideshpande2908@gmail.com>
2024-05-01 21:03:48 +0530

    Code: selection sort

---
+++ algorithms/selection.js
@@ -0,0 +1,37 @@
+let arr = [2, 13, 3, 10, 1, 20, 61, 82, 12, 11]
+let len = arr.length
+
+function selection(arr, len) {
+	let minIndex
+	for (let i = 0; i < len - 1; i++) {
+		minIndex = i
+		for (let j = i + 1; j < len; j++) {
+			if (arr[j] < arr[minIndex]) {
+				minIndex = j
+			}
+		}
+
+		let temp = arr[minIndex]
+		arr[minIndex] = arr[i]
+		arr[i] = temp
+	}
+	return arr
+}
+
+console.log(selection(arr, len))
+
+// First Try 😙
+/* function selection(arr, len) {
+	for (let i = 0; i < len; i++) {
+		currentMinVal = arr[i]
+		for (let j = i; j < len; j++) {
+			if (currentMinVal > arr[j]) {
+				let temp = arr[j]
+				arr[j] = arr[i]
+				arr[i] = temp
+			}
+		}
+	}
+
+	return arr
+} */
